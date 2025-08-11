eb17b03e10d30dcd64c027f7d508bd4f97a73514
sachin98765 <sm3938722@gmail.com>
2025-08-11 22:13:08 +0530

    file-handling , os module

--- 04_http-server/index.js
+++ 04_http-server/index.js
@@ -1,33 +1,9 @@
 const http = require('http');
-const fs = require('fs');
-const url =require('url')
 
 const myServer = http.createServer((req, res)=> {
-    if(req.url ==="/favicon.ico") return res.end()
-    const log=`${Date.now()}: ${req.url} New Req Received \n`
-    const myUrl = url.parse(req.url,true)
-    console.log(myUrl)
-    fs.appendFile("log.txt", log,(err,data)=>{
-        switch(myUrl.pathname){
-            case '/':res.end("Homepage")
-            break;
-
-            case '/about':
-                const userName = myUrl.query.myNAme
-                res.end(`Hi, ${userName}, Welcome to the About page`)
-            break;
-
-            case '/search':
-                const searchQuery = myUrl.query.search_query
-                res.end("Search results for: " + searchQuery)
-            break;
-
-            default: res.end("404 Not Found")
-        }
-        // res.end("Hello from the server Again")
-    })
-    // console.log(req.headers)
+   console.log("New Req Rec..");
+   res.end("Hello From Server");
     
 }) 
 
-myServer.listen(8000, () =>console.log("Server is running on port 8000"))
+myServer.listen(8000, () =>console.log("Server is running on port 8000"));
