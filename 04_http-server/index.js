1ddadfdb0eb8a36ad8f043aad2ac0194ae6b4bba
sachin98765 <sm3938722@gmail.com>
2025-08-14 22:45:59 +0530

    HTTP Server

--- 04_http-server/index.js
+++ 04_http-server/index.js
@@ -1,9 +1,29 @@
 const http = require('http');
+const fs=require('fs');
+const url=require("url");
 
 const myServer = http.createServer((req, res)=> {
-   console.log("New Req Rec..");
-   res.end("Hello From Server");
+   if(req.url === "/favicon.ico") return res.end();
+   const log=`${Date.now()}: ${req.url} New Req Received \n`;
+   const myUrl= url.parse(req.url, true);
+   console.log(myUrl);
+   fs.appendFile("log.txt",log, (err, data)=>{
+      switch(myUrl.pathname){
+         case '/': res.end("HomePage");
+         break;
+
+         case '/about':
+            const username=myUrl.query.name;
+            const roll=myUrl.query.roll;
+             res.end(`Hi, ${username},${roll}`);
+         break;
+
+         default: res.end("404 Not Found");
+         break;
+
+      }
+   });
     
-}) 
+}) ;
 
 myServer.listen(8000, () =>console.log("Server is running on port 8000"));
