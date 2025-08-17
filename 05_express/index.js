b50fd76cb98d33c5d049ce7388f21c84201dc8bc
sachin98765 <sm3938722@gmail.com>
2025-08-17 08:04:20 +0530

    express js

--- 05_express/index.js
+++ 05_express/index.js
@@ -1,9 +1,7 @@
-const http = require('http');
-const fs = require('fs');
-const url =require('url')
-const express = require('express')
 
+const express = require('express')
 const app = express();
+
 
 app.get('/', (req, res) =>{
     return res.send('Hello from home page')
@@ -12,7 +10,6 @@ app.get('/', (req, res) =>{
 app.get('/about', (req, res) =>{
     return res.send('Hello from about page'+" hey "+req.query.name+" my age "+req.query.age)
 })
-
 app.listen(8002, () =>console.log("Server is running on port 8002"))
 
 
